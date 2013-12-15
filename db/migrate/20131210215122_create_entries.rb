e1d594eaf666ce4713687a3bdeceb67602cc6992
bengwinter <ben.g.winter@gmail.com>
2013-12-15 17:23:21 -0500

    edited entries migration

--- db/migrate/20131210215122_create_entries.rb
+++ db/migrate/20131210215122_create_entries.rb
@@ -4,11 +4,12 @@ class CreateEntries < ActiveRecord::Migration
       t.text :zip_code
       t.integer :salary
       t.integer :asset_value
+      t.integer :parents_asset_value
       t.text :race
       t.text :gender
       t.text :education
-      t.text :industry
-      #add user guesses columns to save
+      t.integer :guess_income_rank
+      t.integer :guess_wealth_rank
       t.timestamps
     end
   end
