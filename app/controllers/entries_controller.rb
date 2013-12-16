674eddc5a745e4cb764f16229e9db7f4ed1e5555
bengwinter <ben.g.winter@gmail.com>
2013-12-16 13:03:29 -0500

    edited controller to have correct variable names to create entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,7 +3,7 @@ class EntriesController < ApplicationController
   end
 
   def submit
-    Entry.create(zip_code: params["zip_code"], race: params["race"], gender: params["gender"], education: params["education"], salary: params["salary"], assets: params["assets"], inherited_assets: params["inherited_assets"], guess_income_rank: params["salary_guess"], guess_wealth_rank: params["wealth_guess"], self_happiness_descr: params["self_happiness_descr"], self_wealth_descr: params["self_wealth_descr"])
+    Entry.create(zip_code: params["zip_code"], race: params["race"], gender: params["gender"], education: params["education"], salary: params["salary"], assets: params["assets"], inherited_assets: params["inherited_assets"], salary_guess: params["salary_guess"], wealth_guess: params["wealth_guess"], self_happiness_descr: params["self_happiness_descr"], self_wealth_descr: params["self_wealth_descr"])
     respond_to do |format|
       format.js
     end
