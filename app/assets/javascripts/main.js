58edacc8a6ace4cb82a7be7eb18441304b53a489
bengwinter <ben.g.winter@gmail.com>
2013-12-16 12:25:25 -0500

    adding inputs for new fields

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
     var inherited_assets = $('#inherited-assets').val();
     var salary_guess = $('#salary-guess').val();
     var wealth_guess = $('#wealth-guess').val();
+    var self_wealth_descr = $('#self-wealth-descr').val();
+    var self_happiness_descr = $('#self-happiness-descr').val();
 
     $('#zip-code').val('');
     $('#race').val('');
@@ -20,12 +22,14 @@ $(document).ready(function(){
     $('#inherited-assets').val('');
     $('#salary-guess').val('');
     $('#wealth-guess').val('');   
+    $('#self-wealth-descr').val('');
+    $('#self-happiness-descr').val('');
 
     $.ajax({
       url: 'submit_entry',
       dataType: 'script',
       type: 'POST',
-      data: {zip_code: zip_code, race: race, gender: gender, education: education, salary: salary, assets: assets, inherited_assets: inherited_assets, salary_guess: salary_guess, wealth_guess: wealth_guess}, 
+      data: {zip_code: zip_code, race: race, gender: gender, education: education, salary: salary, assets: assets, inherited_assets: inherited_assets, salary_guess: salary_guess, wealth_guess: wealth_guess, self_happiness_descr: self_happiness_descr, self_wealth_descr: self_wealth_descr}, 
       success: function(){
       }
     });
--- db/migrate/20131210215122_create_entries.rb
+++ db/migrate/20131210215122_create_entries.rb
@@ -9,8 +9,12 @@ class CreateEntries < ActiveRecord::Migration
       t.text :race
       t.text :gender
       t.text :education
+      t.text :self_wealth_desc
+      t.text :self_happiness_desc
       t.integer :guess_income_rank
       t.integer :guess_wealth_rank
+      t.integer :output_income_rank
+      t.integer :output_wealth_rank
       t.timestamps
     end
   end
