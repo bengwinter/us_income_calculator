3c22c9d91975f8688bbae491279eb38d5335c1dc
bengwinter <ben.g.winter@gmail.com>
2013-12-17 09:09:10 -0500

    more styling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,39 +1,68 @@
 $(document).ready(function(){
-  $('#submit-entry').click(function(e){
-    e.preventDefault();
-    var zip_code = $('#zip-code').val();
-    var race = $('#race').val();
-    var gender = $('#gender').val();
-    var education = $('#education-level').val();
-    var salary = $('#salary').val();
-    var assets = $('#assets').val();
-    var inherited_assets = $('#inherited-assets').val();
-    var salary_guess = $('#salary-guess').val();
-    var wealth_guess = $('#wealth-guess').val();
-    var self_wealth_descr = $('#self-wealth-descr').val();
-    var self_happiness_descr = $('#self-happiness-descr').val();
-
-    $('#zip-code').val('');
-    $('#race').val('');
-    $('#gender').val('');
-    $('#education-level').val('');
-    $('#salary').val('');
-    $('#assets').val('');
-    $('#inherited-assets').val('');
-    $('#salary-guess').val('');
-    $('#wealth-guess').val('');   
-    $('#self-wealth-descr').val('');
-    $('#self-happiness-descr').val('');
-
-    $.ajax({
-      url: 'submit_entry',
-      dataType: 'script',
-      type: 'POST',
-      data: {zip_code: zip_code, race: race, gender: gender, education: education, salary: salary, assets: assets, inherited_assets: inherited_assets, salary_guess: salary_guess, wealth_guess: wealth_guess, self_happiness_descr: self_happiness_descr, self_wealth_descr: self_wealth_descr}, 
-      success: function(){
-      }
-    });
-    return false;
-  });
-  
+  Site.setSubmitEvent();
+  Site.sizeForm();
+  Site.pageResizeListeners();
+  Site.animateAbout();
 });
+
+var Site = {
+  sizeForm: function() {
+    $("#container").css('max-height', $(window).height() - $('#bottom-bar').height());
+  },
+
+  pageResizeListeners: function() {
+    $(window).resize(function() {
+      Site.sizeForm();
+    });
+  },
+
+  animateAbout: function() {
+    $('#about').click(function(e){
+      e.preventDefault();
+      if($('#bottom-bar').hasClass('open')) {
+        $('#bottom-bar').removeClass('open').transition({
+          height: "25px"
+        });
+      }else{
+        $('#bottom-bar').addClass('open').transition({
+          height: "100px"
+        });
+      }
+    })
+  },
+
+  animateSubmit: function() {
+    $('#user-input').removeClass('large-12').addClass('large-3');
+    $('.form-column').removeClass('large-4').removeClass('right-border-column').addClass('large-12').addClass('form-column-min');
+    //$('.input-container').removeClass('small-5').addClass('small-3');
+    //$('.input-container-no-columns-class').addClass('small-3 columns');
+    $('.form-subheader').removeClass('entry-form-header').addClass('entry-form-header-min')
+    $('#submit-entry').remove();
+    $('#output').fadeIn();
+  },
+
+  renderData: function(data) {
+    var zipHtml = "<h2>Zip Code: </h2>" + data["zip_code"] + "<h3>Salary Guess: </h3>" + data["salary_guess"]
+    $('#output').append(zipHtml);
+  },
+
+  setSubmitEvent: function() {
+    $('#submit-entry').click(function(e){
+      console.log('i got clicked');
+      e.preventDefault();
+      var zip_code = $('#zip-code').val(), race = $('#race').val(), gender = $('#gender').val(), education = $('#education-level').val(), salary = $('#salary').val(), assets = $('#assets').val(), inherited_assets = $('#inherited-assets').val(), salary_guess = $('#salary-guess').val(), wealth_guess = $('#wealth-guess').val(), self_wealth_descr = $('#self-wealth-descr').val(), self_happiness_descr = $('#self-happiness-descr').val(), age = $('#age').val();
+      var data = {zip_code: zip_code, race: race, gender: gender, education: education, salary: salary, assets: assets, inherited_assets: inherited_assets, salary_guess: salary_guess, wealth_guess: wealth_guess, self_happiness_descr: self_happiness_descr, self_wealth_descr: self_wealth_descr, age: age};
+      
+      $.ajax({
+        url: 'submit_entry',
+        type: 'POST',
+        data: data, 
+        success: function(){
+        }
+      });
+      Site.animateSubmit();
+      Site.renderData(data);
+    });
+
+  }
+}
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,10 +3,8 @@ class EntriesController < ApplicationController
   end
 
   def submit
-    Entry.create(zip_code: params["zip_code"], race: params["race"], gender: params["gender"], education: params["education"], salary: params["salary"], assets: params["assets"], inherited_assets: params["inherited_assets"], salary_guess: params["salary_guess"], wealth_guess: params["wealth_guess"], self_happiness_descr: params["self_happiness_descr"], self_wealth_descr: params["self_wealth_descr"])
-    respond_to do |format|
-      format.js
-    end
+    Entry.create(zip_code: params["zip_code"], race: params["race"], gender: params["gender"], education: params["education"], salary: params["salary"], assets: params["assets"], inherited_assets: params["inherited_assets"], salary_guess: params["salary_guess"], wealth_guess: params["wealth_guess"], self_happiness_descr: params["self_happiness_descr"], self_wealth_descr: params["self_wealth_descr"], age: params["age"])
+    #issue with redirect set default to no
   end
 
 end
