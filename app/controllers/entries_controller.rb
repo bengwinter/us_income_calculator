52ebca781a4bb4d6f4aa7985024f87f09ba07b71
bengwinter <ben.g.winter@gmail.com>
2013-12-19 21:40:14 -0600

    updated csv file with final data

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,16 +12,16 @@ class EntriesController < ApplicationController
     geo = "allallgeography" + params["geo_zone"]
     city = "allallgeography" + params["city_type"]
     education = "allalleducation" + params["education"]
-    # race = "all" + params["race"] + "allall"
+    race = "all" + params["race"] + "allall"
     gender_education = params["gender"] + "alleducation" + params["education"]
     gender_age = params["gender"] + "allage" + params["age"]
-    # gender_race = params["gender"] + params["race"] + "allall"
+    gender_race = params["gender"] + params["race"] + "allall"
     gender_race_education = params["gender"] + params["race"] + "education" + params["education"]
     gender_race_age = params["gender"] + params["race"] + "age" + params["age"]
     race_education = "all" + params["race"] + "education" + params["education"]
     race_age = "all" + params["race"] + "age" + params["age"]
-    keys << all << gender << age << geo << city << education << gender_education << gender_age << gender_race_education << gender_race_age << race_education << race_age
-    #<< race << gender_race
+    keys << all << gender << age << geo << city << education << gender_education << gender_age << gender_race_education << gender_race_age << race_education << race_age << race << gender_race
+    binding.pry
     keys.each do |key|
       p = 0
       l = DATA_2013[key].length
